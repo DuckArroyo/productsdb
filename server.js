0c33c2a7c56b040a039e13ded50405c30b88a75a
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-17 17:54:18 -0800

    before tutoring

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.use(routes);
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   console.log(`App listening on port ${PORT}!`);
-  app.listen(PORT, () => console.log("Now serving em beets!"));
+  app.listen(PORT, () => console.log("Now serving em deep beets!"));
 });
